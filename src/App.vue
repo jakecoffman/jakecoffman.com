fc64630d3291fa7692d7f527dd7af83cf6c1239e
jake <jake@jakecoffman.com>
2019-02-03 21:38:57 -0600

    more init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,110 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header>
+      jakecoffman.com
+    </header>
+
+    <nav>
+      <div class="face">
+        <img alt="Jake Coffman" src="./assets/logo.png" class="logo">
+        <div class="facetext">
+          <em>Software Developer</em>
+          <span>Web, Android, Games</span>
+        </div>
+      </div>
+
+      <ul>
+        <li>
+          <router-link to="/">Intro</router-link>
+        </li>
+        <li>
+          <router-link to="/projects">Projects</router-link>
+        </li>
+        <li>
+          <router-link to="/qa">Q&A</router-link>
+        </li>
+        <li>
+          <a href="https://twitter.com/nill">@nill on Twitter</a>
+        </li>
+        <li>
+          <a href="https://github.com/jakecoffman">jakecoffman on Github</a>
+        </li>
+      </ul>
+    </nav>
+
+    <main>
+      <router-view></router-view>
+    </main>
+
+    <footer>
+      <a href="https://github.com/jakecoffman/jakecoffman.com">Fork me on Github</a>
+    </footer>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  $green: #259b24;
+
+  html, body {
+    margin: 0;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+
+    display: grid;
+    grid-template-rows: 40px auto 30px;
+    grid-template-columns: 262px 1fr;
+    height: 100vh;
+  }
+
+  header, footer {
+    grid-column-start: 1;
+    grid-column-end: 3;
+
+
+    background: $green;
+    color: white;
+    padding: 0.5rem;
+
+    a, a :hover, a :visited {
+      text-decoration: none;
+      color: white;
+    }
+  }
+
+  nav {
+    background: #eeeeee;
+  }
+
+  main {
+    padding: 0.5rem;
+    font-size: large;
+  }
+
+  .face {
+    display: flex;
+    justify-content: space-evenly;
+    padding: 0.5rem;
+    font-size: smaller;
+  }
+
+  .facetext {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    em {
+      font-weight: bolder;
+    }
+  }
+
+  .logo {
+    display: block;
+    width: 64px;
+    height: 64px;
+    border-radius: 100%;
+  }
 </style>
