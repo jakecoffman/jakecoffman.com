2b46c217f1a16f84b5a2ed51e16cca112fdca462
jake <jake@jakecoffman.com>
2019-02-09 09:14:50 -0600

    update intro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,35 @@
     <h2>Hello world!</h2>
 
     <p>Hi, I’m Jake Coffman and I am a developer in St. Louis Missouri.
+    </p>
 
-    <p>I have a Bachelors in Computer Science and have worked professionally in C++, Python, JavaScript, Java, Groovy, Ruby, Go, and C#.
+    <p>I have a Bachelors in Computer Science and have worked professionally in C++, Python, JavaScript,
+       Java, Groovy, Ruby, Go, and C#.
+    </p>
 
     <h2>Experience</h2>
-    I started out working on low level security libraries and various security related GUIs at Cerner.
+    <p>I started out working on low level security libraries and various security related GUIs at Cerner.
+    </p>
 
-    Then I moved on to doing low level network programming on a 3-D distributed simulation of fighter jets at Boeing. I started to pick up web programming while I was there, so my next job was a startup called Appistry.
+    <p>Then I moved on to doing low level network programming on a 3-D distributed simulation of fighter jets
+       at Boeing. I started to pick up web development while I was there, too.
+    </p>
 
-    At Appistry I helped create an ETL system that took HL7 data and transformed it into SQL where it could be searched from the web. I also did various other “Big Data” web interfaces.
+    <p>I then moved to a startup called Appistry where I helped create an ETL system that took HL7 data and
+      transformed it into SQL, and created a search interface. I also did various other "Big Data" web interfaces.
+    </p>
 
-    Now I am at World Wide Technology where I maintain a sales-oriented system of web applications that allow customers to launch virtual machines and connect to them via their browser.
+    <p>Now I am at World Wide Technology where I specialize in RESTful API design and
+      UI development with VueJS.
+    </p>
 
     <h2>Interests</h2>
-    I like game development and have been working on games in my free time. Game development is refreshing and completely unlike web development which I have been doing for my day job. Working in 2D and 3D with physics engines is a fun challenge.
+    <p>I like game development and have been working on games in my free time. Game development is
+      refreshing and completely unlike web development which I have been doing for my day job. Working in 2D and 3D with physics engines is a fun challenge.
+    </p>
 
-    I also like web development as a way to expose what I do to potentially billions of people.
+    <p>I also like web development as a way to expose what I do to potentially billions of people.
+    </p>
   </div>
 </template>
 
